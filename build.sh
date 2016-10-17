1faf67805b5893db3b1b2c7d8a8c914d26d72d31
lieff <lieff@users.noreply.github.com>
2016-10-17 18:55:25 +0300

    update tcc

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-gcc -s -Os -flto -fno-stack-protector nanovg/nanovg.c svg.c lunzip.c -L. -I. -Inanovg -o svg -lm -lglfw -lGL -ltcc3 -ldl
+gcc -s -Os -flto -fno-stack-protector nanovg/nanovg.c svg.c lunzip.c -L. -I. -Inanovg -o svg -lm -lglfw -lGL -ltcc2 -ldl
