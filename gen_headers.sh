1e5f536e2841cdd29fd74154893de10800dfb1f8
lieff <lieff@users.noreply.github.com>
2017-05-04 14:19:18 +0300

    add code minification

--- gen_headers.sh
+++ gen_headers.sh
@@ -1,6 +1,9 @@
 echo '#include <math.h>' >> allh.h
 echo '#define NANOSVG_ALL_COLOR_KEYWORDS' >> allh.h
-cat include/*.h nanovg/nanosvg.h nanovg/nanovg.h lvg_header.h >> allh.h
+cat include/*.h nanovg/nanosvg.h nanovg/nanovg.h lvg_header.h >> allh_fat.h
+gcc -fpreprocessed -dD -E -P allh_fat.h > allh.h
+#python ../cminify/minifier.py allh_fat.h > allh.h
 dd if=/dev/zero bs=1 count=1 >> allh.h
 xxd -i allh.h >all.h
 rm allh.h
+rm allh_fat.h
