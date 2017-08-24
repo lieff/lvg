4a5b52cdd85c8b6b847699cfb56b44c581d61537
lieff <lieff@users.noreply.github.com>
2017-08-25 00:34:31 +0300

    enable asan in debug

--- build_debug.sh
+++ build_debug.sh
@@ -1,5 +1,5 @@
-gcc -g -O0 -Wall nanovg/nanovg.c adpcm.c lvg_audio.c lvg.c svgb.c swf.c lunzip.c \
+gcc -g -O0 -Wall -fsanitize=address nanovg/nanovg.c adpcm.c lvg_audio.c lvg.c svgb.c swf.c lunzip.c \
 swftools/lib/*.c swftools/lib/modules/*.c swftools/lib/as3/*.c mp3/minimp3.c \
 render/*.c \
 video/ffmpeg/ffmpeg_dec.c \
--L. -I. -Inanovg -Iswftools/lib -Imp3 -D_DEBUG -D_GNU_SOURCE -o lvg -lm -lglfw -lGL -ltcc2 -ldl -lSDL2 -lavcodec -lavutil
+-L. -I. -Inanovg -Iswftools/lib -Imp3 -D_DEBUG -D_GNU_SOURCE -o lvg -lasan -lm -lglfw -lGL -ltcc2 -ldl -lSDL2 -lavcodec -lavutil
