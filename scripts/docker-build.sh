2f294b1ddcb548ce1a53efed1d150ed40108bbc5
lieff <lieff@users.noreply.github.com>
2017-08-30 17:25:13 +0300

    install upx

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 apt-get update -qq
-apt-get install -qq -y libglfw3-dev libsdl2-dev libavcodec-dev
+apt-get install -qq -y libglfw3-dev libsdl2-dev libavcodec-dev upx-ucl
 cd /lvg
 ./build.sh
