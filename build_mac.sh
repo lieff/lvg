e57c21d765777f1a2dfe51efada2dccd6c52c03f
lieff <lieff@users.noreply.github.com>
2017-08-31 13:10:47 +0300

    try -lglfw for old brew

--- build_mac.sh
+++ build_mac.sh
@@ -3,7 +3,7 @@ swftools/lib/*.c swftools/lib/modules/*.c swftools/lib/as3/*.c mp3/minimp3.c \
 render/*.c \
 video/ffmpeg/ffmpeg_dec.c \
 -I/usr/local/include -L/usr/local/lib \
--L. -I. -Inanovg -Iswftools/lib -Imp3 -DNDEBUG -D_GNU_SOURCE -o lvg_macos -lm -lglfw3 -ltcc -ldl -lSDL2 -lavcodec -lavutil \
+-L. -I. -Inanovg -Iswftools/lib -Imp3 -DNDEBUG -D_GNU_SOURCE -o lvg_macos -lm -lglfw -ltcc -ldl -lSDL2 -lavcodec -lavutil \
 -framework OpenGL `pkg-config --static --libs glfw3`
 upx -9 ./lvg_macos
 zip -9 -u lvg_macos.zip lvg_macos
