99349757096304d5a35b978e4ef059a21a35eaaa
lieff <lieff@users.noreply.github.com>
2017-09-05 14:59:37 +0300

    add wget dep

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 apt-get update -qq
-apt-get install -qq -y git mercurial cmake yasm libglfw3-dev libsdl2-dev libavcodec-dev mingw-w64 zip
+apt-get install -qq -y git mercurial cmake yasm libglfw3-dev libsdl2-dev libavcodec-dev mingw-w64 zip wget
 cd /lvg
 ./build.sh
 ./build_win.sh
