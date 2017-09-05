34053cc0f38f6bb85c290417bebbf2bf1bda2dc0
lieff <lieff@users.noreply.github.com>
2017-09-05 12:43:47 +0300

    fix macos

--- build_mac.sh
+++ build_mac.sh
@@ -1,4 +1,4 @@
-gcc -Os -flto -std=gnu99 -Wall -fno-stack-protector -ffunction-sections -fdata-sections -Wl,--gc-sections nanovg/nanovg.c adpcm.c lvg_audio.c lvg.c svgb.c swf.c lunzip.c \
+gcc -Os -flto -std=gnu99 -Wall -fno-stack-protector -ffunction-sections -fdata-sections -Wl,-dead_strip nanovg/nanovg.c adpcm.c lvg_audio.c lvg.c svgb.c swf.c lunzip.c \
 swftools/lib/*.c swftools/lib/modules/*.c swftools/lib/as3/*.c mp3/minimp3.c \
 render/*.c \
 swf/avm1.c \
