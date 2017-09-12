3a96fd807254dacc23bccfbc26c6c140051000c7
lieff <lieff@users.noreply.github.com>
2017-09-12 22:47:43 +0300

    fix assert

--- swf/swf.c
+++ swf/swf.c
@@ -930,8 +930,8 @@ static void parsePlacements(TAG *firstTag, character_t *idtable, LVGMovieClip *c
             if (flags & PF_RATIO) target->ratio = p.ratio;
             if (flags & PF_CLIPDEPTH) target->clipdepth = p.clipdepth;
             if (flags & PF_NAME)
-            {   // name to access sprite is action script
-                assert(sprite_type == idtable[p.id].type || none_type == idtable[p.id].type/*text*/);
+            {   // name to access objects in action script
+                assert(sprite_type == idtable[p.id].type || button_type == idtable[p.id].type || none_type == idtable[p.id].type/*text*/);
                 group->group_labels = realloc(group->group_labels, (group->num_group_labels + 1)*sizeof(group->group_labels[0]));
                 LVGGroupLabel *gl = group->group_labels + group->num_group_labels++;
                 gl->name = strdup(p.name);
