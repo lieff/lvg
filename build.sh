6b8398b33f853735733425016082a5a545420b99
lieff <lieff@users.noreply.github.com>
2017-12-01 22:16:41 +0300

    use old ffmpeg api with circle CI

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ set -e
 
 . scripts/build-sdl.sh linux
 
-gcc -s -Os -flto -std=gnu99 -Wall -fno-asynchronous-unwind-tables -fno-stack-protector -ffunction-sections -fdata-sections -Wl,--gc-sections nanovg/nanovg.c src/lvg.c src/svgb.c src/lunzip.c \
+CFLAGS="-s -Os -flto -std=gnu99 -Wall -fno-asynchronous-unwind-tables -fno-stack-protector -ffunction-sections -fdata-sections -Wl,--gc-sections nanovg/nanovg.c src/lvg.c src/svgb.c src/lunzip.c \
 audio/*.c \
 render/*.c \
 platform/*.c \
@@ -10,7 +10,14 @@ scripting/tcc/script_tcc.c \
 swf/*.c swf/swftools/lib/*.c swf/swftools/lib/modules/*.c swf/swftools/lib/as3/*.c audio/mp3_keyj/minimp3.c \
 video/ffmpeg/ffmpeg_dec.c \
 -LSDL/build-linux -ISDL/include -Lscripting/tcc -I. -Isrc -Iscripting/tcc -Inanovg -Iswf/swftools/lib \
--DNDEBUG -D_GNU_SOURCE -DLVG_INTERPOLATE -o lvg -Wl,-Map=lvg.map -lm -ltcc2 -ldl -lSDL2 -lavcodec -lavutil -lpthread
+-DNDEBUG -D_GNU_SOURCE -DLVG_INTERPOLATE"
+
+if [ "$CIRCLECI" = "true" ]; then
+CFLAGS="$CFLAGS -DOLD_API"
+fi
+
+gcc $CFLAGS -o lvg -Wl,-Map=lvg.map -lm -ltcc2 -ldl -lSDL2 -lavcodec -lavutil -lpthread
+
 objcopy --remove-section=.comment --remove-section=.note* --remove-section=.gnu.version --remove-section=.eh_frame* --remove-section=.jcr ./lvg
 scripts/compress.sh ./lvg
 if [ "$TRAVIS" = "true" ]; then
