ae91f38a459e29c0796f109c415ab7e2c9dfe17a
lieff <lieff@users.noreply.github.com>
2018-05-18 19:47:13 +0300

    do not build glfw, use SDL by default

--- build_mac.sh
+++ build_mac.sh
@@ -8,8 +8,7 @@ scripting/tcc/script_tcc.c \
 swf/*.c swf/swftools/lib/*.c swf/swftools/lib/modules/*.c swf/swftools/lib/as3/*.c \
 video/ffmpeg/ffmpeg_dec.c \
 -I/usr/local/include -L/usr/local/lib \
--I. -Isrc -Iscripting/tcc -Inanovg -Iswf/swftools/lib -DNDEBUG -D_GNU_SOURCE -DLVG_INTERPOLATE -o lvg_macos -lm -lglfw -ltcc -ldl -lSDL2 -lavcodec -lavutil \
-`pkg-config --static --libs glfw3`
+-I. -Isrc -Iscripting/tcc -Inanovg -Iswf/swftools/lib -DNDEBUG -D_GNU_SOURCE -DLVG_INTERPOLATE -o lvg_macos -lm -ltcc -ldl -lSDL2 -lavcodec -lavutil
 upx --best --ultra-brute ./lvg_macos
 if [ "$TRAVIS" = "true" ]; then
     zip -9 -u lvg_macos.zip lvg_macos
--- build_win.sh
+++ build_win.sh
@@ -1,13 +1,13 @@
 set -e
 
-if [ ! -d "glfw" ]; then
-  git clone --depth=1 https://github.com/glfw/glfw
-  mkdir glfw/build
-  cd glfw/build
-  cmake -DCMAKE_TOOLCHAIN_FILE=../../windows/mingw64.cmake -DGLFW_BUILD_EXAMPLES=0 -DGLFW_BUILD_TESTS=0 -DGLFW_BUILD_DOCS=0 ..
-  make
-  cd ../../
-fi
+#if [ ! -d "glfw" ]; then
+#  git clone --depth=1 https://github.com/glfw/glfw
+#  mkdir glfw/build
+#  cd glfw/build
+#  cmake -DCMAKE_TOOLCHAIN_FILE=../../windows/mingw64.cmake -DGLFW_BUILD_EXAMPLES=0 -DGLFW_BUILD_TESTS=0 -DGLFW_BUILD_DOCS=0 ..
+#  make
+#  cd ../../
+#fi
 
 . scripts/build-sdl.sh win
 
@@ -33,9 +33,9 @@ swf/*.c swf/swftools/lib/*.c swf/swftools/lib/modules/*.c swf/swftools/lib/as3/*
 render/*.c \
 windows/mman.c \
 video/ffmpeg/ffmpeg_dec.c \
--Lglfw/build/src -LSDL/build-win -Lvideo/ffmpeg/FFmpeg/libavcodec -Lvideo/ffmpeg/FFmpeg/libavutil \
+-LSDL/build-win -Lvideo/ffmpeg/FFmpeg/libavcodec -Lvideo/ffmpeg/FFmpeg/libavutil \
 -I. -Isrc -Iscripting/tcc -Inanovg -Iswf/swftools/lib \
--Iglfw/include -ISDL/include -Ivideo/ffmpeg/FFmpeg \
+-ISDL/include -Ivideo/ffmpeg/FFmpeg \
 -DNDEBUG -D_GNU_SOURCE -DLVG_INTERPOLATE -o lvg_win.exe -Wl,-Map=lvg.map -lm -lopengl32 -lSDL2 -lavcodec -lavutil -luser32 -lgdi32 -lwinmm -limm32 -lole32 -loleaut32 -lshell32 -lbcrypt -lversion
 scripts/compress.sh ./lvg_win.exe
 if [ "$TRAVIS" = "true" ]; then
