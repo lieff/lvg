61c083bba5ca5c1815aecaf663bb57753ffe7ea0
lieff <lieff@users.noreply.github.com>
2018-08-13 15:00:14 +0300

    macos: add frameworks

--- build_mac.sh
+++ build_mac.sh
@@ -5,13 +5,12 @@ scripts/build-ffmpeg-pc.sh macos
 . build_src.sh
 
 gcc -Os -flto -std=gnu99 -Wall -fno-stack-protector -ffunction-sections -fdata-sections -Wl,-dead_strip $SRC \
--I/usr/local/include -L/usr/local/lib \
 -I. -Isrc -Inanovg -Iswf/swftools/lib \
 -LSDL/build-macos -ISDL/include -I. \
 -Ivideo/ffmpeg/FFmpeg -Ivideo/ffmpeg/FFmpeg/build-macos -Lvideo/ffmpeg/FFmpeg/build-macos/libavcodec -Lvideo/ffmpeg/FFmpeg/build-macos/libavutil \
--DNDEBUG -D_GNU_SOURCE -DLVG_INTERPOLATE -o lvg_macos -lm -ldl -lSDL2 -lavcodec -lavutil \
--framework Foundation -framework AVFoundation -framework CoreAudio -framework CoreVideo -framework CoreMedia -framework CoreGraphics -framework VideoToolbox \
--framework OpenGL -framework AudioToolbox -framework IOKit -framework AppKit -framework ForceFeedback
+-DNDEBUG -D_GNU_SOURCE -DLVG_INTERPOLATE -o lvg_macos -lm -ldl -liconv -lSDL2 -lavcodec -lavutil \
+-framework Foundation -framework AVFoundation -framework CoreAudio -framework CoreVideo -framework CoreMedia -framework CoreGraphics -framework CoreServices -framework VideoToolbox \
+-framework OpenGL -framework AudioToolbox -framework IOKit -framework AppKit -framework ForceFeedback -framework Carbon -framework Cocoa
 upx --best --ultra-brute ./lvg_macos
 if [ "$TRAVIS" = "true" ]; then
     zip -9 -u lvg_macos.zip lvg_macos
