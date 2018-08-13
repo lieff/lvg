cd0aada105b0fc737805ace72f6845f777d34846
lieff <lieff@users.noreply.github.com>
2018-08-13 14:24:04 +0300

    macos: add frameworks

--- build_mac.sh
+++ build_mac.sh
@@ -9,7 +9,8 @@ gcc -Os -flto -std=gnu99 -Wall -fno-stack-protector -ffunction-sections -fdata-s
 -I. -Isrc -Inanovg -Iswf/swftools/lib \
 -LSDL/build-macos -ISDL/include -I. \
 -Ivideo/ffmpeg/FFmpeg -Ivideo/ffmpeg/FFmpeg/build-macos -Lvideo/ffmpeg/FFmpeg/build-macos/libavcodec -Lvideo/ffmpeg/FFmpeg/build-macos/libavutil \
--DNDEBUG -D_GNU_SOURCE -DLVG_INTERPOLATE -o lvg_macos -lm -ldl -lSDL2 -lavcodec -lavutil
+-DNDEBUG -D_GNU_SOURCE -DLVG_INTERPOLATE -o lvg_macos -lm -ldl -lSDL2 -lavcodec -lavutil \
+-framework Foundation -framework AVFoundation -framework CoreAudio -framework CoreVideo -framework CoreMedia -framework VideoToolbox -framework OpenGL -framework AudioToolbox
 upx --best --ultra-brute ./lvg_macos
 if [ "$TRAVIS" = "true" ]; then
     zip -9 -u lvg_macos.zip lvg_macos
